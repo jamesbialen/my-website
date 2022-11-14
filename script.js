cfdf91ec855c3946688243c6640bc9e8a87421c4
jamesbialen <116704205+jamesbialen@users.noreply.github.com>
2022-11-14 19:13:27 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const hamburger = document.getElementsByClassName("hamburger")[0]
+const mobileNavs = document.getElementsByClassName("nav-links")[0]
+hamburger.addEventListener("click", () => {
+  mobileNavs.classList.toggle("active")
+})
+
+'<jamesbialen>.github.io/' + '<https://github.com/jamesbialen/file-C-Users-logical-Desktop-portfolio-index.html.git>/'
